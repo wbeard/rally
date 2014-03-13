af16e867c828f8819330dbcfd628f245f9eb564d
William Beard <disenno@gmail.com>
2014-03-13 15:45:21 -0500

    fix spec description

--- specs/numberToString-spec.js
+++ specs/numberToString-spec.js
@@ -4,7 +4,7 @@ describe("Correct thousands transaction", function() {
     it("should display Two thousand five hundred twenty three and 04/100 dollars", function() {
       expect(numberToStringRep(2523.04)).toBe("Two thousand five hundred twenty three and 04/100 dollars");
     });
-    it("should return undefined", function() {
+    it("should expect an error to be thrown", function() {
       expect(numberToStringRep).toThrow(new Error("Must pass an argument and must be a number"));
     });
 });
